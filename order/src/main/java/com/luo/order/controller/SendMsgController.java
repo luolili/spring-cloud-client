e7bf227de764df7025cc7de9d1dd9de6e4c67eee
Administrator <2084267015@qq.com>
2019-07-13 21:45:11 +0800

    cloud stream

--- order/src/main/java/com/luo/order/controller/SendMsgController.java
+++ order/src/main/java/com/luo/order/controller/SendMsgController.java
@@ -1,5 +1,6 @@
 package com.luo.order.controller;
 
+import com.luo.order.dto.OrderDTO;
 import com.luo.order.message.StreamClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.support.MessageBuilder;
@@ -14,10 +15,18 @@ public class SendMsgController {
     @Autowired
     private StreamClient streamClient;
 
-    @GetMapping("/send")
+   /* @GetMapping("/send")
     public void process() {
         streamClient.output()
                 .send(MessageBuilder.withPayload("now:" + new Date()).build());
+    }*/
 
+   //传一个对象
+    @GetMapping("/send")
+    public void process() {
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setOrderId("12345");
+        streamClient.output()
+                .send(MessageBuilder.withPayload(orderDTO).build());
     }
 }
--- order/src/main/java/com/luo/order/message/StreamReceiver.java
+++ order/src/main/java/com/luo/order/message/StreamReceiver.java
@@ -1,5 +1,6 @@
 package com.luo.order.message;
 
+import com.luo.order.dto.OrderDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
@@ -15,8 +16,15 @@ public class StreamReceiver {
      * stream receiver: now:Sat Jul 13 21:08:19 CST 2019
      * @param message
      */
-    @StreamListener("myMessage01")
+   /* @StreamListener("myMessage01")
     public void process(Object message) {
+        log.info("stream receiver: {}", message);
+    }*/
+
+   //接受一个对象:OrderDTO
+    //注释掉接收者，看发送的数据的格式
+    @StreamListener("myMessage01")
+    public void process(OrderDTO message) {
         log.info("stream receiver: {}", message);
     }
 }
